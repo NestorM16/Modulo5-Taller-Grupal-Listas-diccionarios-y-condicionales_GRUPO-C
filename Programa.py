7eb8c242a365a1ee398617ffc1fcec73f3a7b90d
Eliecer Santana Rodriguez <eliecer.santana@uip.edu.pa>
2026-07-14 10:40:07 -0500

    Update Programa.py

--- Programa.py
+++ Programa.py
@@ -1,2 +1,11 @@
 #lista de nombre
 nombres = ["Ana", "Carlos", "María", "Juan", "Sofía"]
+
+#Diccionario de edades
+edades = {
+  "Ana": 30,
+  "Carlos":25,
+  "María":28,
+  "Juan":35,
+  "Sofía":22,
+}
